0af905bd5006f9f58843a91ae3caa2b9f9836b58
jaft24 <jtesgera@cord.edu>
2023-12-25 20:33:57 -0600

    Add Test Stage to Pipeline

--- src/network/axiosInstance.ts
+++ src/network/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://pokedex-backend-project.fly.dev",
-  timeout: 7000,
+  timeout: 30000,
 });
 
 export default axiosInstance;
